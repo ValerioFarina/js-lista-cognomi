ccd4d21bdb55c9c013b81e023e840170545160fd
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-19 14:10:37 +0200

    Creato un array contenente dei cognomi in ordine casuale

---
+++ main.js
@@ -0,0 +1,9 @@
+/* CONSEGNA: Creare un array contenente dei cognomi in ordine casuale.
+             Chiedere all'utente il suo cognome e inserirlo nella lista dei cognomi.
+             Stampare la lista dei cognomi in ordine alfabetico e comunicare all'utente la posizione in cui si trova il suo cognome (dopo averlo riordinato). */
+
+
+// creo un array contenente dei cognomi in ordine casuale e lo salvo in una variabile
+var lastNames = ['rossi', 'colombo', 'ferrari', 'costa', 'fontana', 'santoro', 'bernardi']
+
+console.log(lastNames);
